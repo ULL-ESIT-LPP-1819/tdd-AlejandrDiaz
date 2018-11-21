bd49beba74e221daec584cbb3169f123a51b2815
alu0100761948 <alu0100761948@ull.edu.es>
2018-11-21 20:36:55 +0000

    añadiendo gitgnore, gemfile, etc. resto de ficheros modificados

---
+++ P8/README.md
@@ -0,0 +1,3 @@
+## Práctica 8: Desarrollo Dirigido por Pruebas (TDD) utilizando Bundler y Rspec. Utilizando herencia. 
+
+#### Autor: Alejandro Díaz Cueca
---
+++ P8/valornutricional/lib/valornutricional.rb
@@ -0,0 +1,7 @@
+require "valornutricional/version"
+require "valornutricional/valornutricional"
+require "valornutricional/lista"
+
+module ValorNutricional
+  # Your code goes here...
+end
